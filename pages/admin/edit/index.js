2bf925866ac92a38b6349fc0048e07414498e3ba
Abiud Cantu <abiudneftali@gmail.com>
2021-01-02 20:18:24 -0600

    Initial commit

---
+++ pages/admin/edit/index.js
@@ -0,0 +1,28 @@
+import { useEffect, useState } from 'react';
+import Link from 'next/link';
+import axios from 'axios';
+
+const List = () => {
+  const [entries, setEntries] = useState([]);
+
+  useEffect(async () => {
+    const res = await axios.get('/api/entries');
+    setEntries(res.data.entriesData);
+  }, []);
+
+  return (
+    <div>
+      <h1>Entries</h1>
+      {entries.map(entry => (
+        <>
+          <Link href={`/admin/edit/${entry.id}`}>
+            <a>{entry.title}</a>
+          </Link>
+          <br/>
+        </>
+      ))}
+    </div>
+  );
+};
+
+export default List;
